b8ff5539c68e79e71b2fb05cc7c979530457d51b
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-21 06:20:07 +0000

    Variables

---
+++ Js_basics/variables.js
@@ -0,0 +1,14 @@
+const account_Id =14453;
+let accountEmail="rohitsoniofficial43@gmail.com";
+var accountPassword ="12345";
+var accountCity ="jaipur";
+without_variale ="timepass"; //still give not error
+
+//var is a global scope
+//let is  local scope
+
+// console.log(account_Id);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([account_Id,accountEmail,accountPassword,accountCity,without_variale]);
