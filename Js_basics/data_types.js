f2117b80bd25462955f94cedd751e74483d90984
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-21 06:44:44 +0000

    Data Types

---
+++ Js_basics/data_types.js
@@ -0,0 +1,7 @@
+"use strict";// treat all JS code as newer Version
+// alert("Hello Bhai sabe");//we are using node js not a browser
+
+console.log(3 + 3);
+//spaces is avoided by js automatically
+//code readability should be high
+
