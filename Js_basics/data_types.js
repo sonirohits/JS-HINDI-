02f22fbd2ff0369c69d2d4b309d234eab2fc51e8
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-21 08:08:22 +0000

    data types

--- Js_basics/data_types.js
+++ Js_basics/data_types.js
@@ -5,3 +5,18 @@ console.log(3 + 3);
 //spaces is avoided by js automatically
 //code readability should be high
 
+let name  ="Hitesh";  //string type data type
+let age=18; // number
+let islogedIn =true; //Boolean type  means true or false
+//NULL it is  data type and stand alone value
+
+// undefined is a means variable declare kiya he lekin value assign nai ki he
+
+//null   means: suppose hamne ek request bheji temperature chahiye koi city ki to return me kuch nai aya to vo null he.
+
+//symbol is also a data type to find uniquness
+
+//Object
+console.log(typeof(islogedIn));  //booleans
+console.log(typeof (null)); //object this is a famous  javascript interview questio 
+console.log(typeof(undefined)); //undefind
