596284f0b19382b1eb42ee7649a50b86212a3132
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-25 15:05:13 +0000

    Function_!

---
+++ JS_3/function.js
@@ -0,0 +1,22 @@
+// function SayName(){
+//     console.log('R');
+//     console.log('o');
+//     console.log('H');
+//     console.log('I');
+//     console.log('T');
+// }
+// SayName();
+
+function add(n1,n2){
+    return n1 + n2;
+}
+// add(100,100);
+console.log(add(100,100));
+
+//default values  in function
+
+function defaulst(name ='Rohit Soni Himanshukumar'){
+console.log(name);
+}
+// defaulst('Rudra Soni');  //Rudra Soni
+defaulst(); //Rohit Soni Himanshukumar
