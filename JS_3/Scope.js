b2291c9b73927a89fe07aca683f8c1a06e25eab9
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-25 15:57:37 +0000

    Scope

---
+++ JS_3/Scope.js
@@ -0,0 +1,9 @@
+
+if(true){
+    let a =10;
+    const b = 20;
+    var c =30;
+}
+//console.log(a);  //   it is not a accessable it is local scope
+//console.log(b); //    it is not a accessable it is local scope
+console.log(c); //      it is accessble because var is global scoped he
