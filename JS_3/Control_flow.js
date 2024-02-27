0fb65ffbdcefa3fdef2ecbb9f8a0990e76e41625
Soni Rohit Himanshukumar <114281620+sonirohits@users.noreply.github.com>
2024-02-27 11:10:02 +0000

    Controle Flow

---
+++ JS_3/Control_flow.js
@@ -0,0 +1,52 @@
+// console.log("Controle Flow In Js");
+// if(true){
+// return'this is true';
+// }
+// let tem1 ="";
+// let tem2 =2379;
+// if(tem1!==tem2){
+//     console.log('This is True');
+// }
+// console.log(typeof tem1); //string 
+// console.log(typeof tem2); //number
+
+// console.log("Enter the month");
+
+// //Swith Case
+// const month =10;
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Enter right value");
+//     break;
+// }
+
+
+//truthy and falsy value
+
+const userEmail ='rohitsoni@gmail.com';
+if(userEmail){
+    console.log("Gotht the useremail");
+}
+else{
+    console.log("Not goting useremail");
+}
+//output Goth the useremaail hamne man  liya ki string (username) true he
+
+//is username is empty then not goting useremail   usko falsy man liya jata he
+
+//falsy values
+
+// false,0,-0,0n,"",null,undefined,NAN
